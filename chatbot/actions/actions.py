e0e06f4e90e9c407b47bc075bcdd554e1059c704
JackFarmer132 <jackfarmer858@gmail.com>
2020-11-06 09:52:30 +0000

    added slots and types for catching user information and storing it

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -9,8 +9,10 @@
 
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet, EventType
+from rasa_sdk.types import DomainDict
 
 
 class ActionHelloWorld(Action):
