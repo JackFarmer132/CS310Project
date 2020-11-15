7e1c938428fd96733d0b0e64eca8f2fee58850a5
JackFarmer132 <jackfarmer858@gmail.com>
2020-11-15 19:27:18 +0000

    removed postcode detection apart from regex

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -4,9 +4,6 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
@@ -14,39 +11,14 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk import Tracker, FormValidationAction
 from rasa_sdk.types import DomainDict
 
-# class ValidateTestForm(FormValidationAction):
-#     def name(self) -> Text:
-#         return "validate_test_form"
-#
-#     @staticmethod
-#     def service_db() -> List[Text]:
-#         # database of the supported emegency service types
-#         return ["police", "ambulance", "fire department"]
-#
-#     def validate_service_type(self, slot_value: Any, dispatcher: CollectingDispatcher,
-#                         tracker: Tracker, domain: DomainDict) -> Dict[Text, Any]:
-#
-#         # if empty then no valid service type
-#         if not slot_value:
-#             return {"service_type": None}
-#
-#         # check each type is in the valid range
-#         for s in slot_value:
-#             if not (s.lower() in self.service_db()):
-#                 return {"service_type": None}
-#
-#         # only here if valid so return as such
-#         return {"service_type": slot_value}
-#
-#     def validate_name(self, slot_value: Any, dispatcher: CollectingDispatcher,
-#                         tracker: Tracker, domain: DomainDict) -> Dict[Text, Any]:
-#         return {"name": slot_value}
-#
-#
-#     def validate_phone_number(self, slot_value: Any, dispatcher: CollectingDispatcher,
-#                         tracker: Tracker, domain: DomainDict) -> Dict[Text, Any]:
-#         return {"phone_number": slot_value}
+# for api calls
+import urllib.request as req
+import json
 
+
+# gets data from api calls
+def getAPIResult(url):
+    return json.loads(req.urlopen(url).read())
 
 class ValidateServiceForm(FormValidationAction):
 
@@ -57,14 +29,15 @@ class ValidateServiceForm(FormValidationAction):
         return "validate_service_form"
 
     @staticmethod
-    # hot list of common emergencies and their associated service types for
+    # hotlist of common emergencies and their associated service types for
     # derriving the required emergency service
     def emergency_hot_list() -> Dict[Text, List[Text]]:
-        hotlist = {"stabbing": ["police", "ambulance"]}
+        hotlist = {"stabbing": ["police", "ambulance"],
+                   "fire": ["ambulance", "fire department"]}
         return hotlist
 
     def validate_service_type(self, slot_value: Any, dispatcher: CollectingDispatcher,
-                        tracker: Tracker, domain: DomainDict) -> Dict[Text, Any]:
+                              tracker: Tracker, domain: DomainDict) -> Dict[Text, Any]:
         # if empty then no valid service type
         if not slot_value:
             return {"service_type": None}
@@ -80,7 +53,7 @@ class ValidateServiceForm(FormValidationAction):
         return {"service_type": slot_value}
 
     def validate_emergency_details(self, slot_value: Any, dispatcher: CollectingDispatcher,
-                                  tracker: Tracker, domain: DomainDict) -> Dict[Text, Any]:
+                                   tracker: Tracker, domain: DomainDict) -> Dict[Text, Any]:
 
         # if empty then no valid set of emergency details
         if not slot_value:
@@ -120,3 +93,27 @@ class ValidateServiceForm(FormValidationAction):
 
         return {"emergency_details": slot_value,
                 "service_type": self.explicit_service_type}
+
+class ValidatePoliceForm(FormValidationAction):
+
+    def name(self) -> Text:
+        return "validate_police_form"
+
+    def validate_street_address(self, slot_value: Any, dispatcher: CollectingDispatcher,
+                                tracker: Tracker, domain: DomainDict) -> Dict[Text, Any]:
+        #
+        print(hello)
+
+    def validate_postcode(self, slot_value: Any, dispatcher: CollectingDispatcher,
+                                tracker: Tracker, domain: DomainDict) -> Dict[Text, Any]:
+        print(slot_value)
+        print(slot_value[0])
+        print(slot_value[1])
+        # url = "https://api.postcodes.io/postcodes/{}".format(slot_value[0])
+        url = "https://api.postcodes.io/postcodes/NN11 0GH"
+        # remove possible space in postcode
+        url = url.replace(" ", "")
+        print(url)
+        result = json.loads(req.urlopen(url).read())
+
+        print(result)
